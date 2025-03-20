db52d1cb106feee4b75c5f0545a18d5b8189ff87
fengchenxue <840814316@qq.com>
2025-03-20 07:56:59 +0000

    SamplingDistributions

--- RTBase/Sampling.h
+++ RTBase/Sampling.h
@@ -32,31 +32,46 @@ public:
 	static Vec3 uniformSampleHemisphere(float r1, float r2)
 	{
 		// Add code here
-		return Vec3(0, 0, 1);
+		float phi = 2.0f * M_PI * r1;
+		float z = r2;
+		float r = sqrtf(std::max(0.0f, 1.0f - z * z));
+		float x = r * cosf(phi);
+		float y = r * sinf(phi);
+		return Vec3(x, y, z);
 	}
 	static float uniformHemispherePDF(const Vec3 wi)
 	{
 		// Add code here
-		return 1.0f;
+		return 1.0f / (2.0f * M_PI);
 	}
 	static Vec3 cosineSampleHemisphere(float r1, float r2)
 	{
 		// Add code here
-		return Vec3(0, 0, 1);
+		float r = sqrtf(r1);
+		float phi = 2.0f * M_PI * r2;
+		float x = r * cosf(phi);
+		float y = r * sinf(phi);
+		float z = sqrtf(std::max(0.0f, 1.0f - x*x-y*y));
+		return Vec3(x, y, z);
 	}
 	static float cosineHemispherePDF(const Vec3 wi)
 	{
 		// Add code here
-		return 1.0f;
+		return wi.z / M_PI;
 	}
 	static Vec3 uniformSampleSphere(float r1, float r2)
 	{
 		// Add code here
-		return Vec3(0, 0, 1);
+		float phi = 2.0f * M_PI * r1;
+		float z = 2.0f * r2 - 1.0f;
+		float r = sqrtf(std::max(0.0f, 1.0f - z * z));
+		float x = r * cosf(phi);
+		float y = r * sinf(phi);
+		return Vec3(x, y, z);
 	}
 	static float uniformSpherePDF(const Vec3& wi)
 	{
 		// Add code here
-		return 1.0f;
+		return 1.0f/(4.0f*M_PI);
 	}
 };
